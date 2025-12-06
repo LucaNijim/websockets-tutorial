8716ba74c5e47cb6600b7cab8805b8b7176d5182
Luca <lnijim@nd.edu>
2025-12-06 17:18:31 -0500

    updated through part 2 of tutorial

--- app.py
+++ app.py
@@ -1,13 +1,135 @@
 import asyncio
+from websockets.asyncio.server import serve, broadcast
+import json
+import secrets
 
-import websockets
-from websockets.asyncio.server import serve
-from websockets.exceptions import ConnectionClosedOK
+from connect4 import PLAYER1, PLAYER2, Connect4
+
+
+JOIN = {}
+WATCH = {}
+
+async def play(websocket, game, player, connected):
+    async for message in websocket:
+        event_in = json.loads(message)
+        print('Recieved event:', event_in)
+        if event_in["type"] == "play":
+            print(f'Event type: play')
+            print(f'Player {player}')
+            try:
+                row = game.play(player, event_in["column"])
+                broadcast(connected, json.dumps({
+                        "type": "play", 
+                        "row": row, 
+                        "column": event_in["column"], 
+                        "player": player}))
+                
+                if game.winner is not None:
+                    broadcast(connected, json.dumps({
+                            "type": "win", 
+                            "message": f"Game Over! Winner is {game.winner}"
+                        }))
+                    
+            except ValueError as e:
+                await websocket.send(json.dumps({"type": "error", "message": str(e)}))
+        else:
+            raise Exception(f"Unexpected event type: {event_in['type']}")
+
+async def catchup(websocket, game):
+    print('Running catchup')
+    for move in game.moves:
+        event = {
+            "type": "play",
+            "player": move[0], 
+            "column": move[1], 
+            "row": move[2], 
+        }
+        await websocket.send(json.dumps(event))
+
+async def start(websocket):
+    # initialize game 
+    game = Connect4()
+    # initialize set of connecteds
+    connected = {websocket}
+    # add this to global dict
+    join_key = secrets.token_urlsafe(8)
+    watch_key = secrets.token_urlsafe(8)
+    JOIN[join_key] = game, connected
+    WATCH[watch_key] = game, connected
+    
+    try:
+        # send secret to client
+        # so they can invite their friends
+        event = {
+            "type": "init",
+            "join": join_key, 
+            "watch": watch_key
+        }
+        await websocket.send(json.dumps(event))
+        
+        # temp ( for testing )
+        print(f"First player started game at {join_key}")
+        await play(websocket, game, PLAYER1, connected)
+    finally:
+        # clear the join thing to avoid memory leaks
+        print(f"Player 1 exiting game at {join_key}")
+        del JOIN[join_key]
+
+async def error(websocket, message):
+    event = {
+        "type": "error",
+        "message": message
+    }
+    await websocket.send(json.dumps(event))
+    
+async def join(websocket, join_key):
+    # find game
+    try:
+        game, connected = JOIN[join_key]
+    except KeyError:
+        await error(websocket, f"No join key {join_key}")
+        return
+    
+    # register to add
+    connected.add(websocket)
+    try: 
+        # testing
+        print(f"Second player joined game {id(game)} at {join_key}")
+        await catchup(websocket, game)
+        await play(websocket, game, PLAYER2, connected)
+    finally:
+        print(f"Player 2 exiting game at {join_key}")
+        connected.remove(websocket)
+
+async def watch(websocket, watch_key):
+    try:
+        game, connected = WATCH[watch_key]
+    except KeyError:
+        await error(websocket, f"No watch key {watch_key}")
+        return
+    
+    connected.add(websocket)
+    try:
+        print(f"Spectator joined game {id(game)} at {watch_key}")
+        await catchup(websocket, game)
+        await websocket.wait_closed()
+    finally:
+        print(f"Spectator exiting game at {watch_key}")
+        connected.remove(websocket)
 
 
 async def handler(websocket):
-    async for message in websocket:
-        print(message)
+    message = await websocket.recv()
+    event = json.loads(message)
+    assert event["type"] == "init"
+    
+    if "join" in event:
+        await join(websocket, event["join"])
+    elif "watch" in event:
+        await watch(websocket, event["watch"])
+    else:
+        # first player starts
+        await start(websocket)
 
 async def main():
     async with serve(handler, "", 8001) as server:
--- main.js
+++ main.js
@@ -6,8 +6,28 @@ window.addEventListener("DOMContentLoaded", () => {
     createBoard(board);
     // initialize websocket connection
     const websocket = new WebSocket("ws://localhost:8001/");
-    sendMoves(board, websocket);
+    initGame(websocket);
+    receiveEvents(board, websocket);
+    // enable to send moves if not spectator
+    const params = new URLSearchParams(window.location.search);
+    if (params.size === 0 || params.has("join")) {
+        sendMoves(board, websocket);
+    }
 });
+
+function initGame(websocket) {
+    websocket.addEventListener("open", () => {
+        const params = new URLSearchParams(window.location.search);
+        let event = {"type": "init"};
+        if (params.has("join")) {
+            event.join = params.get("join");
+        } else if (params.has("watch")) {
+            event.watch = params.get("watch");
+        }
+        console.log(`sending event ${JSON.stringify(event)}`);
+        websocket.send(JSON.stringify(event));
+    });
+}
 
 function sendMoves(board, websocket) {
     board.addEventListener("click", ({ target }) => {
@@ -20,5 +40,34 @@ function sendMoves(board, websocket) {
             column: parseInt(column, 10),
         };
         websocket.send(JSON.stringify(event));
+    });
+}
+
+function showMessage(message) {
+    window.setTimeout(() => window.alert(message), 50);
+}
+
+function receiveEvents(board, websocket) {
+    websocket.addEventListener("message", ({ data }) => {
+        const event = JSON.parse(data);
+        switch (event.type) {
+            case "play":
+                playMove(board, event.player, event.column, event.row);
+                break;
+            case "win":
+                showMessage(`Player ${event.player} wins!`);
+                websocket.close(1000);
+                break;
+            case "error":
+                showMessage(event.message);
+                break;
+            case "init":
+                document.querySelector(".join").href = "?join=" + event.join;
+                document.querySelector(".watch").href = "?watch=" + event.watch;
+                break;
+            default:
+                throw new Error(`Unsupported event type: ${event.type}`);
+                
+        }
     });
 }
